2232083f62f3b1b6f32f9a67bd5cd67069a0acce
Yaroslaff Fedin <yaroslaff.fedin@refinery29.com>
2013-02-23 23:11:38 +0800

    Add deployment script

---
+++ deploy.sh
@@ -0,0 +1,27 @@
+mkdir ../alexandrfedorov.ru
+cp -R output/* ../alexandrfedorov.ru
+cp -R output/ru/* ../alexandrfedorov.ru
+
+mkdir ../alexandrfedorov.com
+cp -R output/* ../alexandrfedorov.com
+cp -R output/en/* ../alexandrfedorov.com
+
+cd ../alexandrfedorov.ru
+echo alexandrfedorov.ru > CNAME
+git init
+git checkout -b gh-pages
+git checkout gh-pages
+git remote add origin git@github.com:alexandrfedorov/alexandrfedorov.github.com.git -f
+git add .
+git commit -m "Update russian version of the website"
+git push origin gh-pages -f
+
+cd ../alexandrfedorov.com
+echo alexandrfedorov.com > CNAME
+git init
+git checkout -b gh-pages
+git checkout gh-pages
+git remote add origin git@github.com:alexandrfedorov/alexandrfedorov.github.com.git -f
+git add .
+git commit -m "Update english version of the website"
+git push origin gh-pages -f
