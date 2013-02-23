d8e3c124db5b83f371a9cc90b05386cc392cc49e
Yaroslaff Fedin <yaroslaff.fedin@refinery29.com>
2013-02-23 23:16:15 +0800

    update deployment script

--- deploy.sh
+++ deploy.sh
@@ -11,7 +11,7 @@ echo alexandrfedorov.ru > CNAME
 git init
 git checkout -b gh-pages
 git checkout gh-pages
-git remote add origin git@github.com:alexandrfedorov/alexandrfedorov.github.com.git -f
+git remote add origin git@github.com:alexandrfedorov/ru.git -f
 git add .
 git commit -m "Update russian version of the website"
 git push origin gh-pages -f
@@ -21,7 +21,7 @@ echo alexandrfedorov.com > CNAME
 git init
 git checkout -b gh-pages
 git checkout gh-pages
-git remote add origin git@github.com:alexandrfedorov/alexandrfedorov.github.com.git -f
+git remote add origin git@github.com:alexandrfedorov/en.git -f
 git add .
 git commit -m "Update english version of the website"
 git push origin gh-pages -f
